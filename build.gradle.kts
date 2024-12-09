ef1cafa489b8bb7fdc6fa0a52a3920701ef3dd96
Kurumi Muto <p20080mk@gm.tsuda.ac.jp>
2024-12-09 15:27:59 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    val kotlinVersion: String by System.getProperties()
+    kotlin("plugin.serialization") version kotlinVersion
+    kotlin("multiplatform") version kotlinVersion
+    val kvisionVersion: String by System.getProperties()
+    id("io.kvision") version kvisionVersion
+}
+
+version = "1.0.0-SNAPSHOT"
+group = "com.mtkrm"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+// Versions
+val kotlinVersion: String by System.getProperties()
+val kvisionVersion: String by System.getProperties()
+
+kotlin {
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                outputFileName = "main.bundle.js"
+                sourceMaps = false
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets["jsMain"].dependencies {
+        implementation("io.kvision:kvision:$kvisionVersion")
+        implementation("io.kvision:kvision-bootstrap:$kvisionVersion")
+    }
+    sourceSets["jsTest"].dependencies {
+        implementation(kotlin("test-js"))
+        implementation("io.kvision:kvision-testutils:$kvisionVersion")
+    }
+}
