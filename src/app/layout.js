9529f09bab88023ea29c4634f12f3eadc5b25cd3
Kurumi Muto <p20080mk@gm.tsuda.ac.jp>
2024-12-11 18:02:53 +0900

    fix font import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import Footer from '../components/Footer';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
